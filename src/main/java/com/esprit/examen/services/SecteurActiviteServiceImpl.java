23ca73a7795e312b4da8bef765e0aa8baf8626df
OussemaAbderrahmen <oussema.abderrahmen@esprit.tn>
2022-10-19 17:31:43 +0100

    code smells fix 15

--- src/main/java/com/esprit/examen/services/SecteurActiviteServiceImpl.java
+++ src/main/java/com/esprit/examen/services/SecteurActiviteServiceImpl.java
@@ -31,8 +31,8 @@ public class SecteurActiviteServiceImpl implements ISecteurActiviteService{
 
 	@Override
 	public SecteurActivite updateSecteurActivite(SecteurActivite sa) {
-		secteurActiviteRepository.save(sa);
-		return sa;
+		return secteurActiviteRepository.save(sa);
+		
 	}
 
 	@Override
